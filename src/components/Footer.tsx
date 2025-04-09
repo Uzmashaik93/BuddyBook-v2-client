5589f7707c707ab110063c30ccaebd411d8af837
uzma shaik <uzma97910@gmail.com>
2025-04-09 16:14:09 +0200

    feat: add Footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,50 @@
+import { Github } from "lucide-react";
+import React from "react";
+
+const Footer: React.FC = () => {
+  return (
+    <footer className="bg-gray-50 text-black pt-16 pb-8">
+      <div className="container mx-auto px-4">
+        <div className="grid grid-cols-1 md:grid-cols-2  gap-8 mb-2">
+          <div>
+            <h3 className="text-lg font-bold mb-4 flex justify-evenly">
+              Buddy Book
+              <Github className="hover:text-pink-400" />
+            </h3>
+            <p className="text-gray-700">
+              Your digital slambook to create and share memories with friends.
+            </p>
+          </div>
+
+          <div>
+            <h3 className="text-lg font-bold mb-4">Quick Links</h3>
+            <ul className="space-y-2">
+              <li>
+                <a
+                  href="#"
+                  className="text-gray-700 hover:text-pink-500 transition"
+                >
+                  Home
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  className="text-gray-700 hover:text-pink-500 transition"
+                >
+                  Features
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        <div className=" border-gray-700 pt-2 text-center text-gray-400 text-sm">
+          <p>&copy; 2025 Buddy Book. All rights reserved.</p>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,74 +1,135 @@
 import { NavLink, useLocation, useNavigate } from "react-router-dom";
-import { House, Info, LogOut, UsersRound } from "lucide-react";
+import { House, Info, LogOut, Menu, UsersRound, X } from "lucide-react";
 
 import logo from "../assets/images/logo/BuddyBook-final.png";
 
 import "../components/Navbar.css";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { AuthContext } from "../context/auth.context";
 
 function Navbar() {
   const { isAuthenticated, logout, user } = useContext(AuthContext);
+  const [isOpen, setIsOpen] = useState(false);
   const navigate = useNavigate();
   const location = useLocation();
   const currentPath = location.pathname;
   return (
-    <div className="navbar-container text-sm flex justify-between items-center bg-white shadow-md px-6 py-3 top-0 w-full z-50">
-      {/* Logo */}
-      <NavLink to="/">
-        <img src={logo} alt="Logo" className="h-10 w-auto -rotate-10" />
-      </NavLink>
-
-      {/* Navigation Links */}
-      <div className="flex items-center gap-6">
-        <h4>Welcome {user?.username}</h4>
-
-        {currentPath !== "/" && (
-          <NavLink
-            to="/"
-            className="flex items-center gap-2 text-gray-700 transition-colors duration-300 hover:text-pink-500 group"
-          >
-            <House
-              className="transition-colors duration-200 group-hover:text-pink-500"
-              color="currentColor"
-              size={20}
-            />
-            <span className="font-medium">Home</span>
-          </NavLink>
-        )}
-
-        {isAuthenticated && currentPath !== "/teams" && (
-          <NavLink
-            to="/teams"
-            className="flex items-center gap-0.5 text-gray-700 transition-colors duration-300 hover:text-pink-500 group"
-          >
-            <UsersRound size={18} />
-            <span className="font-medium">My Teams</span>
-          </NavLink>
-        )}
-
-        {isAuthenticated && (
-          <button
-            onClick={() => {
-              logout();
-              navigate("/");
-              console.log("Logged out");
-            }}
-            className="flex text-sm items-center gap-0.5 text-gray-700 transition-colors duration-300 hover:text-pink-500 group"
-          >
-            <LogOut size={18} />
-            <span className="font-medium">LogOut</span>
-          </button>
-        )}
-
-        <NavLink
-          to="/about"
-          className="flex items-center text-sm gap-0.5 text-gray-700 transition-colors duration-300 hover:text-pink-500 group"
-        >
-          <Info size={18} />
-          <span className="font-medium">About</span>
+    <div className="navbar-container text-sm bg-white shadow-md py-2 top-0 w-full z-50">
+      <div className="flex justify-between items-center px-7">
+        {/* Logo */}
+        <NavLink to="/">
+          <img src={logo} alt="Logo" className="h-10 w-auto -rotate-10" />
         </NavLink>
+        {isOpen && (
+          <h4 className="text-gray-700 font-bold md:ml-auto">
+            Welcome {user?.username}
+          </h4>
+        )}
+        {/* Hamburger (Mobile) */}
+        <button
+          className="md:hidden text-gray-700"
+          onClick={() => setIsOpen(!isOpen)}
+        >
+          {isOpen ? <X size={24} /> : <Menu size={24} />}
+        </button>
+
+        {/* Desktop Nav */}
+        <div className="hidden md:flex items-center gap-6">
+          <h4 className="text-gray-700 font-bold md:ml-auto">
+            Welcome {user?.username}
+          </h4>
+          {currentPath !== "/" && (
+            <NavLink
+              to="/"
+              className="flex items-center gap-2 text-gray-700 transition hover:text-pink-500 group"
+            >
+              <House className="group-hover:text-pink-500" size={20} />
+              <span className="font-medium">Home</span>
+            </NavLink>
+          )}
+
+          {isAuthenticated && currentPath !== "/teams" && (
+            <NavLink
+              to="/teams"
+              className="flex items-center gap-1 text-gray-700 transition hover:text-pink-500 group"
+            >
+              <UsersRound size={18} />
+              <span className="font-medium">My Teams</span>
+            </NavLink>
+          )}
+
+          {isAuthenticated && (
+            <button
+              onClick={() => {
+                logout();
+                navigate("/");
+              }}
+              className="flex items-center gap-1 text-gray-700 transition hover:text-pink-500 group"
+            >
+              <LogOut size={18} />
+              <span className="font-medium">Logout</span>
+            </button>
+          )}
+
+          <NavLink
+            to="/about"
+            className="flex items-center gap-1 text-gray-700 transition hover:text-pink-500 group"
+          >
+            <Info size={18} />
+            <span className="font-medium">About</span>
+          </NavLink>
+        </div>
       </div>
+
+      {/* Mobile Nav */}
+      {isOpen && (
+        <div className="w-full bg-white shadow-md md:hidden p-5 flex flex-col gap-4 ">
+          {currentPath !== "/" && (
+            <NavLink
+              to="/"
+              onClick={() => setIsOpen(false)}
+              className="flex items-center gap-2 text-gray-700 hover:text-pink-500"
+            >
+              <House size={20} />
+              <span>Home</span>
+            </NavLink>
+          )}
+
+          {isAuthenticated && currentPath !== "/teams" && (
+            <NavLink
+              to="/teams"
+              onClick={() => setIsOpen(false)}
+              className="flex items-center gap-2 text-gray-700 hover:text-pink-500"
+            >
+              <UsersRound size={18} />
+              <span>My Teams</span>
+            </NavLink>
+          )}
+
+          {isAuthenticated && (
+            <button
+              onClick={() => {
+                logout();
+                navigate("/");
+                setIsOpen(false);
+              }}
+              className="flex items-center gap-2 text-gray-700 hover:text-pink-500"
+            >
+              <LogOut size={18} />
+              <span>Logout</span>
+            </button>
+          )}
+
+          <NavLink
+            to="/about"
+            onClick={() => setIsOpen(false)}
+            className="flex items-center gap-2 text-gray-700 hover:text-pink-500"
+          >
+            <Info size={18} />
+            <span>About</span>
+          </NavLink>
+        </div>
+      )}
     </div>
   );
 }
