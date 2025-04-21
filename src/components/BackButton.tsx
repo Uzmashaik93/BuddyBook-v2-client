53a6b8f1e81cfb3ca33234b1230b74a2be822634
uzma shaik <uzma97910@gmail.com>
2025-04-21 18:38:26 +0200

    fix: add margin-top to BackButton component

--- src/components/BackButton.tsx
+++ src/components/BackButton.tsx
@@ -14,7 +14,7 @@ function BackButton({ text, to }: BackButtonProps) {
   };
 
   return (
-    <div className="mb-2">
+    <div className="mb-2 mt-6">
       <button
         onClick={handleClick}
         className="text-gray-600 text-sm hover:text-gray-900 flex items-center"
